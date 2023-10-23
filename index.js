63debefee5b89c626d934e79791cf88b7f6b9ecd
isaacodex-max <145682294+isaacodex-max@users.noreply.github.com>
2023-10-23 17:54:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+};
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+};
+
+//-------------- click me Section //
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("container-1");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+
+
